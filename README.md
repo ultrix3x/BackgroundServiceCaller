2927b3b5a5b0396ebcb8390f97392b4e0501108e
Thomas Björk <tb@bytecode.se>
2014-08-02 22:34:00 +0200

    Added documentation and code comments

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
-BackgroundServiceCaller
-=======================
+# BackgroundServiceCaller
 
-This is a skeleton class for using JBackgroundService
+This is a implementation in PHP for using BackgroundService written in javascript. (BackgroundService is designed to run in node.js).
+
+## Requirements
+This class uses Fuse Array to handle the configuration.
+- [PHPClasses](http://www.phpclasses.org/package/8706)
+- [GitHub](https://github.com/ultrix3x/FuseArray)
+
+The class uses JBackgroundService as the service manager.
+- [JSClasses](http://www.jsclasses.org/package/358)
+- [GitHub](https://github.com/ultrix3x/JBackgroundService)
+
+## BackgroundServiceCaller
+
+### Functions
+
+#### AssignIni($ini)
+Loads the configuration into an ConfigArray (an extension of the class FuseArray)
+
+#### ConvertCharset($data, $fromCharset, $toCharset)
+Convert the data in the mixed-type variable $data from the charset defined 
+in $fromCharset to the charset defined in $toCharset.
+
+#### Init()
+Initialize the object.
+
+Returns a string containing the name of the class used in the call.
+
+#### UDPCall($service, $data, $charset = false)
+Create a call to the service defined in configuration. The call is made over a datagram socket.
+
+There is no response to this call.
+
+#### TCPCall($service, $data, $charset = false)
+Create a call to the service defined in configuration and wait for the response. The call is made over a tcp socket.
+
+#### TCPAddQueue($service, $data, $charset = false)
+Make a call to the tcp service. But instead of waiting for the result the process returns directly and this function returns aa string that contains the job id for the process on the server.
+
+#### TCPCheckQueue($id)
+This function uses the job id created by TCPAddQueue to check the status for the job.
+
+If the function returns:
+- 0 then the job has been created but not started. (This should not be possible since the status is changed from 0 to 1 before the TCPAddQueue returns.)
+- 1 then the job has been started.
+- 2 then the job is finished.
+
+#### TCPGetQueue($id)
+This function calls the job (defined by the id) and waits until the job is done (has the status 2).
+
+## Using multiple service hosts
+The class is written to use a single configuration. The configuration can use several different services since it connects the name of a service to a section of the configuration.
+
+But there is a way to use multiple configurations.
+
+By extending the base class BackgroundServiceCaller it is possible to load one configuration for each extended class. This is since the internal system uses the name of the called class to store the configuration internally.
+```php
+<?php
+include('./bgcaller.php');
+class DummyService1 extends BackgroundServiceCaller {}
+class DummyService2 extends BackgroundServiceCaller {}
+class DummyService3 extends BackgroundServiceCaller {}
+
+BackgroundServiceCaller::AssignIni('./config0.ini');
+DummyService1::AssignIni('./config1.ini');
+DummyService2::AssignIni('./config2.ini');
+DummyService3::AssignIni('./config3.ini');
+
+// Now we can use four (4) different configurations.
+```
--- bgcaller.php
+++ bgcaller.php
@@ -3,7 +3,13 @@ class BackgroundServiceCaller {
   protected static $encFunc = null;
   protected static $config = array();
   
+  /**
+   * Assign a configuration to the class
+   * @param type $ini ini data as either a string or a filename
+   * @throws Exception if the FuseArray class is missing
+   */
   public static function AssignIni($ini) {
+    // If the class ConfigArray doesn't exist then try to load it
     if(!class_exists('ConfigArray')) {
       if(is_file(__DIR__.'/extends/configarray.php')) {
         include_once(__DIR__.'/extends/configarray.php');
@@ -14,85 +20,162 @@ class BackgroundServiceCaller {
     $class = get_called_class();
     if(!isset(static::$config[$class])) {
       if(class_exists('ConfigArray')) {
+        // If the class exist then load the ini-data
         static::$config[$class] = ConfigArray::ConvertIni($ini);
       } else {
+        // It the class doesn't exist then throw an exception
         throw new Exception('Missing FuseArray. Please include it into project');
       }
     }
   }
   
-  public static function ConvertFromCharset($data, $charset) {
+  /**
+   * Convert data from  one charset to another
+   * @param mixed $data the data to convert
+   * @param type $from the charset to convert from
+   * @param type $to the charset to conver to
+   * @return mixed data in the charset defined by $to
+   */
+  public static function ConvertCharset($data, $from, $to) {
     if(is_string($data)) {
-      return static::$encFunc($data, $charset, 'utf-8');
+      // If data is a string then convert
+      return static::$encFunc($data, $from, $to);
     } elseif(is_array($data) || ($data instanceof ArrayAccess)) {
+      // If data is an array then loop through the array and convert each part
       $newData = array();
       foreach($data as $key => $value) {
-        $key = static::$encFunc($key, $charset, 'utf-8');
-        $newData[$key] = static::ConvertFromCharset($value);
+        $key = static::$encFunc($key, $from, $to);
+        $newData[$key] = static::ConvertCharset($value, $charset);
       }
       return $newData;
     }
   }
   
+  /**
+   * Initialize the class and setup the encoding functions
+   * @return string current class name
+   * @throws Exception when there is no configuration loaded
+   */
   public static function Init() {
-    if(static::$config === null) {
-      throw new Exception('No config available. Please call '.  get_called_class().'::AssignIni() before calling this function.');
+    // Get the called class
+    // This makes it possible to keep different configurations, one for
+    // each extended class.
+    $class = get_called_class();
+    if(!isset(static::$config[$class])) {
+      // If the configuration for this class has been loaded then throw
+      // an exception
+      throw new Exception('No config available. Please call '.get_called_class().'::AssignIni() before calling this function.');
     }
+    // If the encoding function has been assigned then do so.
     if(static::$encFunc === false) {
+      // Use mb_convert_encoding if that exists, otherwise use iconv if
+      // that exists
       if(function_exists('mb_convert_encoding')) {
+        // Create an anonymous function that calls mb_convert_encoding
         static::$encFunc = function($data, $fromEncoding, $toEncoding) {
           return mb_convert_encoding($data, $toEncoding, $fromEncoding);
         };
       } elseif(function_exists('iconv')) {
+        // Createan anonymous function that calls iconv
         static::$encFunc = function($data, $fromEncoding, $toEncoding) {
           return iconv($fromEncoding, $toEncoding, $data);
         };
       }
     }
+    // Return the class name
+    return $class;
   }
   
+  /**
+   * Make an datagram call to the service
+   * @param string $service the name of the service
+   * @param string $data the data to send to the service
+   * @param string|bool $charset the charset to use for the request. If
+   *           this is set to false then utf-8 is used
+   * @throws Exception if the configuration doesn't contain a tcpurl for
+   *           the service or if the service can't be opened.
+   */
   public static function UDPCall($service, $data, $charset = false) {
-    static::Init();
+    // Call the Init to initialize and to get the current class name
+    $class = static::Init();
     if($charset !== false) {
-      $data = static::ConvertFromCharset($data, $charset, 'utf-8');
+      // If the charset is set then convert the data
+      $data = static::ConvertCharset($data, $charset, 'utf-8');
     }
-    $class = get_called_class();
+    // Get the url to use from the configuration
     $url = static::$config[$class]->Get($service.'.'.'udpurl');
+    // If the url doesn't exist in the configuration
     if($url === null) {
+      // then throw an exception
       throw new Exception('No url defined for this UDP Service ('.$service.')');
     }
+    // Open a datagram socket
     $fp = @stream_socket_client($url, $errno, $errstr);
     if($fp) {
+      // Set the timeout to 50 ms
       @socket_set_timeout($fp, 0, 50);
       if($data !== null) {
+        // If there is data to to write it by combining the name of the 
+        // service and the data combined by at colon (:)
         @fwrite($fp, $service.':'.$data);
+      } else {
+        // Otherwise just write the service name amd a colon (:)
+        @fwrite($fp, $service.':');
       }
+      // Close the connection
       @fclose($fp);
+    } else {
+      // IF the socket couldn't be opend then throw an exception
+      throw new Exception('Can\'t open url');
     }
   }
   
+  /**
+   * Make a tcp call to the service
+   * @param string $service the name of the service
+   * @param string $data the data to send to the service
+   * @param string|bool $charset the charset to use for the request. If
+   *           this is set to false then utf-8 is used
+   * @return string the id for the job prefixed with "id:"
+   * @throws Exception if the configuration doesn't contain a tcpurl for
+   *           the service or if the service can't be opened.
+   */
   public static function TCPCall($service, $data, $charset = false) {
-    static::Init();
+    // Call the Init to initialize and to get the current class name
+    $class = static::Init();
     if($charset !== false) {
-      $data = static::ConvertFromCharset($data, $charset, 'utf-8');
+      // If the charset is set then convert the data
+      $data = static::ConvertCharset($data, $charset, 'utf-8');
     }
-    $class = get_called_class();
+    // Get the url to use from the configuration
     $url = static::$config[$class]->Get($service.'.'.'tcpurl');
+    // If the url doesn't exist in the configuration
     if($url === null) {
+      // then throw an exception
       throw new Exception('No url defined for this TCP Service ('.$service.')');
     }
+    // Open a tcp socket
     $fp = @fsockopen($url);
     if($fp) {
+      // If the socket could be opened then write data to it
+      // combine the name of the service and the data combined by at colon
       fputs($fp, $service.':'.$data);
+      // Set the output variable to an empty string
       $result = '';
+      // While the socket is open then read data
       while(!feof($fp)) {
+        // Read string of maximum 1024 bytes
         $line = @fgets($fp, 1024);
+        // and add it to the output variable
         $result .= $line;
       }
+      // When the socket has been closed by the server then close the file
       fclose($fp);
+      // If the result begins with a number between 400-599 followed by a
+      // colon then the service has returned an error
       if(!preg_match('/^[45][0-9]{2}\:/Us', ltrim($result))) {
         if($charset !== false) {
-          $result = static::ConvertFromCharset($data, 'utf-8', $charset);
+          $result = static::ConvertCharset($data, 'utf-8', $charset);
         }
         return $result;
       }
@@ -102,16 +185,38 @@ class BackgroundServiceCaller {
     return null;
   }
   
+  /**
+   * Add the job to the defined service with some data
+   * @param string $service
+   * @param string $data
+   * @param string|bool $charset
+   * @return string
+   */
   public static function TCPAddQueue($service, $data, $charset = false) {
     $id = static::TCPCall('queue', 'add:'.$service.':'.$data, $charset);
     return $id;
   }
   
+  /**
+   * Check the status of the given job (by id)
+   * Returns a numeric string
+   *  0 = Job has been registered but not yet started
+   *  1 = Job has been started
+   *  2 = Job is done
+   * @param string $id
+   * @return string
+   */
   public static function TCPCheckQueue($id) {
     $data = static::TCPCall('queue', 'check:'.$id);
     return $data;
   }
   
+  /**
+   * Get the result for the given job (by id)
+   * The function will wait until the job is finished.
+   * @param string $id
+   * @return string
+   */
   public static function TCPGetQueue($id) {
     $data = static::TCPCall('queue', 'get:'.$id);
     return $data;
